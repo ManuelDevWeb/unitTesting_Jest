e85c070f945ccd545cce26b0e5bd82653975e03a
ManuelDevWeb <manueldeveloper17@gmail.com>
2021-10-18 22:43:53 -0500

    Se agregaron pruebas al componente Product

---
+++ platzi-store/src/__mocks__/ProductMock.js
@@ -0,0 +1,12 @@
+/* Los mocks son funciones que simulan acciones que deberia hacer nuestra app.
+Este mock permite simular que tenemos un producto */
+
+const ProductMock = {
+  'id': '1',
+  'image': 'https://arepa.s3.amazonaws.com/camiseta.png',
+  'title': 'Camiseta',
+  'price': 25,
+  'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+};
+
+export default ProductMock;
---
+++ platzi-store/src/__test__/components/Product.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+/* mount --> Cuando necesitas el DOM
+   shallow --> Solo necesitas algo particular del componente. No ocupas todo el DOM
+*/
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import ProductMock from '../../__mocks__/ProductMock';
+
+// Importando componente
+import Product from '../../components/Product';
+
+// Describir una serie de pruebas (dentro de un bloque) que utilizaremos en este componente
+describe('<Product />', () => {
+  // Validando si estamos montando Product
+  test('Render del componente Product', () => {
+    const product = shallow(
+      <ProviderMock>
+        <Product />
+      </ProviderMock>,
+    );
+    expect(product.length).toEqual(1);
+  });
+
+  // Simulando funcionamiento del click en el boton comprar
+  test('Comprobar el boton de comprar', () => {
+    const handleAddToCart = jest.fn();
+    const wrapper = mount(
+      <ProviderMock>
+        <Product
+          product={ProductMock}
+          handleAddToCart={handleAddToCart}
+        />
+      </ProviderMock>,
+    );
+    wrapper.find('button').simulate('click');
+    expect(handleAddToCart).toHaveBeenCalledTimes(1);
+  });
+});
