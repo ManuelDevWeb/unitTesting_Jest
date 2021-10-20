72b7a60683e3378d449147df4210f072a4da6c4d
ManuelDevWeb <manueldeveloper17@gmail.com>
2021-10-20 03:30:57 -0500

    Se agregaron pruebas a los actions

---
+++ platzi-store/src/__test__/actions/actions.test.js
@@ -0,0 +1,24 @@
+import actions from '../../actions';
+import ProductMock from '../../__mocks__/ProductMock';
+
+// Describir una serie de pruebas (dentro de un bloque) que utilizaremos en este componente
+describe('Actions', () => {
+  const payload = ProductMock;
+  // Validando el action addToCart
+  test('addToCart Action ', () => {
+    const expected = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+    expect(actions.addToCart(payload)).toEqual(expected);
+  });
+
+  // Validando el action removeFromCart
+  test('removeFromCart', () => {
+    const expected = {
+      type: 'REMOVE_FROM_CART',
+      payload,
+    };
+    expect(actions.removeFromCart(payload)).toEqual(expected);
+  });
+});
